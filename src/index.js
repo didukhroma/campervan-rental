54792abc76d804682abc6f63698ab1f98317e5e5
didukhroma <didukhroma@gmail.com>
2024-05-22 23:28:07 +0300

    fix: fix routing at gh pages

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={<Loader />} persistor={persistor}>
-        <BrowserRouter>
+        <BrowserRouter basename="/campervan-rental">
           <App />
         </BrowserRouter>
       </PersistGate>
