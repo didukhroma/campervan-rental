de705637e328e106a4536dfe4d494cb0aa0a078e
didukhroma <didukhroma@gmail.com>
2024-05-22 20:10:20 +0300

    fix: fix routing

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/campervan-rental/',
   build: { sourcemap: true },
   resolve: {
     alias: {
